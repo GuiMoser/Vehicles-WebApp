d4cb334ba25401a69e9d03a57dc5d379585fe598
Guilherem <guilherem@Mac-mini.local>
2024-03-21 14:48:19 -0300

    web_app test 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web_app
 Projeto 5 TripleTen
+
+===============================
+### Descrição do projeto:
+Esse projeto consiste em um web app com o intuito de analisar os dados de um dataframe de vendas de carros, utilizando-se de histogramas e outros gráficos.
---
+++ app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv('/Users/guilherem/documents/vscode/web_app/vehicles.csv')
+
+st.title("Vehicles Data Explorer")
+
+hist_button = st.button('Criar um Histograma')
+
+if hist_button:
+    st.write('Criando um histograma de preços por tipos.')
+    # Criando um histograma com plotly.express para analisar o preco pelos tipos dos carros.
+    fig1 = px.histogram(df, x='price', color='type', nbins=40, title='Prices by Types',
+                   color_discrete_map={'SUV': 'green', 'truck': 'red', 'sedan': 'deepskyblue', 'pickup': 'yellow', 'coupe': 'chocolate', 
+                                      'wagon': 'cyan', 'mini-van': 'lightblue', 'hatchback': 'turquoise', 'van': 'springgreen', 
+                                      'convertible': 'lime', 'other': 'goldenrod', 'offroad': 'violet', 'bus': 'purple'})
+    st.plotly_chart(fig1, use_container_width=True)
+
+scatter_button = st.button('Criar um Gráfico de Dispersão')
+
+if scatter_button:
+    st.write('Criando um gráfico de dispersão de kilometragem por ano do modelo.')
+    # Criando um gráfico de dispersão para analisar a kilometragem e a condicão por ano de modelo.
+    fig2 = px.scatter(df, x='model_year', y='odometer', title='Odometer by Model Year', color='condition', 
+                 color_discrete_map={'good': 'green', 'like new': 'deepskyblue', 'fair': 'crimson', 'excellent': 'goldenrod', 
+                                     'salvage': 'red', 'new': 'mediumseagreen'})
+    st.plotly_chart(fig2, use_container_width=True)
+
+check_types = df['type'].unique().tolist()
+selected_types = st.sidebar.checkbox('Selecione o tipo de carro para criar um histograma.', check_types)
+df_types = df[df['type'].isin(selected_types)]
+
+if selected_types:
+    fig = px.histogram(df_types, x='type', title='Quantidade de Carros por Tipo')
+    st.plotly_chart(fig, use_container_width=True)
+else:
+    st.write('Por favor, selecione um tipo de carro.')
+
