30fc92ae881bdce868e8d30b5695fe13e93568bc
Guilherme Moser <153304848+GuiMoser@users.noreply.github.com>
2024-03-21 15:34:33 -0300

    Update vehicles.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df = pd.read_csv('/documents/vscode/web_app/vehicles.csv')
+df = pd.read_csv('vehicles.csv')
 
 st.title("Vehicles Data Explorer")
 
